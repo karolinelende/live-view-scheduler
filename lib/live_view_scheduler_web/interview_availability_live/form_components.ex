725a992e7a81c0b9a9acf734892b42f8e00ddc7e
Karoline Lende <karoline.lende@multiverse.io>
2022-11-20 18:40:17 +0000

    update formatter

--- lib/live_view_scheduler_web/interview_availability_live/form_components.ex
+++ lib/live_view_scheduler_web/interview_availability_live/form_components.ex
@@ -3,32 +3,32 @@ defmodule LiveViewSchedulerWeb.InterviewAvailabilityLive.FormComponents do
 
   def show_availability(assigns) do
     ~H"""
-    <div> View mode </div>
+    <div>View mode</div>
     <.week_select selected_week_beginning={@selected_week_beginning} />
-    <button phx-click="toggle-edit-mode"> Edit </button>
+    <button phx-click="toggle-edit-mode">Edit</button>
     """
   end
 
   defp week_select(assigns) do
     ~H"""
     <div>
-    <h3> Week beginning: <%=  @selected_week_beginning %> </h3>
-        <div>
-          <button phx-click="change_week" value="-1">
-            <span>← Previous week</span>
-          </button>
-          <button phx-click="change_week" value="1">
-            <span>Next week →</span>
-          </button>
-        </div>
+      <h3>Week beginning: <%= @selected_week_beginning %></h3>
+      <div>
+        <button phx-click="change_week" value="-1">
+          <span>← Previous week</span>
+        </button>
+        <button phx-click="change_week" value="1">
+          <span>Next week →</span>
+        </button>
+      </div>
     </div>
     """
   end
 
   def edit_availability(assigns) do
     ~H"""
-    <div> Edit mode </div>
-    <button phx-click="toggle-edit-mode"> Cancel </button>
+    <div>Edit mode</div>
+    <button phx-click="toggle-edit-mode">Cancel</button>
     """
   end
 end
