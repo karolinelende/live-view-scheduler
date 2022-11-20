87437679367f171e82d6b38d8a73d4306b87a521
Karoline Lende <karoline.lende@multiverse.io>
2022-11-20 17:16:32 +0000

    mix format

--- test/live_view_scheduler_web/views/error_view_test.exs
+++ test/live_view_scheduler_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule LiveViewSchedulerWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(LiveViewSchedulerWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(LiveViewSchedulerWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
