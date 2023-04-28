8b094025a7ae58f2cfa418afe76491916d1b4331
Karoline Lende <karoline.lende@multiverse.io>
2023-04-28 11:32:23 +0100

    remove unused changeset for now

--- lib/live_view_scheduler/interview_stage.ex
+++ lib/live_view_scheduler/interview_stage.ex
@@ -1,6 +1,5 @@
 defmodule LiveViewScheduler.InterviewStage do
   use Ecto.Schema
-  import Ecto.Changeset
   alias LiveViewScheduler.InterviewAvailability
 
   schema "interview_stages" do
@@ -12,23 +11,5 @@ defmodule LiveViewScheduler.InterviewStage do
       on_replace: :mark_as_invalid
 
     timestamps(type: :utc_datetime_usec)
-  end
-
-  def availability_changeset(
-        %__MODULE__{interview_availabilities: interview_availabilities} = interview_stage,
-        attrs
-      )
-      when is_list(interview_availabilities) do
-    interview_stage
-    |> cast(attrs, [])
-    |> cast_assoc(
-      :interview_availabilities,
-      with: fn interview_availability, attributes ->
-        # adds the related interview_stage to the interview_availability here so we can
-        # validate the interview_availability duration >= interview_stage.duration
-        %{interview_availability | interview_stage: interview_stage}
-        |> InterviewAvailability.create_changeset(attributes)
-      end
-    )
   end
 end
